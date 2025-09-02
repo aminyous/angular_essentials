6e7a01d1b450ba23d208691c39b760cf8b3b7fb8
Amine Hichri <amyn.hichri@gmail.com>
2025-09-02 13:20:14 +0200

    Play with the user component - listing to event with event binding + state changing

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -6,10 +6,17 @@ import { DUMMY_USERS } from '../dummy-users';
   standalone: true,
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
+  selectedUser = DUMMY_USERS[3];
 
-  selectedUser = DUMMY_USERS[4];
+  get imagePath() {
+    return '../../assets/users/' + this.selectedUser.avatar;
+  }
 
+  onSelectUser() {
+    const randomNumber = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser = DUMMY_USERS[randomNumber];
+  }
 }
